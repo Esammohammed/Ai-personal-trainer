173a51441d8473e6a42fc18036d141c03f4ed2d8
Essam Mohammed <71528058+Esammohammed@users.noreply.github.com>
2022-02-19 19:52:19 +0000

    ...

--- biceps curl.py
+++ biceps curl.py
@@ -9,7 +9,7 @@ def calculate_angle(a, b, c):
     a = np.array(a)  # First
     b = np.array(b)  # Mid
     c = np.array(c)  # End
-
+    myname = "goher"
     radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
     angle = np.abs(radians * 180.0 / np.pi)
 
--- main.py
+++ main.py
@@ -7,6 +7,15 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+<<<<<<< HEAD
+<<<<<<< HEAD
+    print('b7b gohar')
+=======
+    print(f'Hi, {name}')
+>>>>>>> de892692e33a1f3ce2e8df0ce2e76286d5e48c03
+=======
+    print(f'Hi, {name}')
+>>>>>>> de892692e33a1f3ce2e8df0ce2e76286d5e48c03
 
 
 # Press the green button in the gutter to run the script.
