33740c599030fe484e40d58f6a7a458f087911de
goherassem <48772933+goherassem@users.noreply.github.com>
2022-02-19 22:00:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print('b7b gohar')
+    print('b7b essam')
 
 
 # Press the green button in the gutter to run the script.
