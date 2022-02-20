8f405d35d0cb949ad00c04819b9d647b46ec32c4
Essam Mohammed <71528058+Esammohammed@users.noreply.github.com>
2022-02-20 20:55:05 +0000

    Class optimization
    I made Pose estimation class which contains calculate angles ,detect and extract points functions to make code clean and reduce code repetition

--- biceps curl.py
+++ biceps curl.py
@@ -1,22 +1,11 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+
+import Pose_Estimation
+
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
-
-
-def calculate_angle(a, b, c):
-    a = np.array(a)  # First
-    b = np.array(b)  # Mid
-    c = np.array(c)  # End
-    myname = "goher"
-    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
-    angle = np.abs(radians * 180.0 / np.pi)
-
-    if angle > 180.0:
-        angle = 360 - angle
-
-    return angle
 
 
 cap = cv2.VideoCapture(0)
@@ -25,23 +14,13 @@ cap = cv2.VideoCapture(0)
 counter = 0
 stage = None
 
+
 ## Setup mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
-        ret, frame = cap.read()
-
-        # Recolor image to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-
-        # Make detection
-        results = pose.process(image)
-
-        # Recolor back to BGR
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
         # Extract landmarks
+        results,image = Pose_Estimation.MakedetectionandExtract(pose,cap);
+
         try:
             landmarks = results.pose_landmarks.landmark
 
@@ -54,7 +33,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                      landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
 
             # Calculate angle
-            angle = calculate_angle(shoulder, elbow, wrist)
+            angle = Pose_Estimation.calculate_angle(shoulder, elbow, wrist)
 
             # Visualize angle
             cv2.putText(image, str(angle),
