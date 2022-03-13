1e80d6bda0b665692069f7815d85c97fc9d87f0f
mariam-moataz <mariammansour556@gmail.com>
2022-03-13 23:42:49 +0200

    HomePage

--- GUI/home.py
+++ GUI/home.py
@@ -17,18 +17,18 @@ def history():
     import history
 
 app=Tk()
-
-
-
 app.title('Home Page ')
 app.geometry('1120x920')
 app.configure(bg='#09c')
 
-HomePage2=Label(app,text='Home Page',font=('bold',35),bg="#09c").place(relx=.5, rely=.2,anchor= CENTER)
+frame1 = Frame(app,width=1000,height=700,highlightbackground='red',highlightthickness=3)
+frame1.grid(row=0,column=0,padx=20,pady=20,ipadx=20,ipady=20)
 
-Exersises=Button(app,text="Exersises",pady=20,padx=20,width=12,borderwidth = 10,command=exersises).place(relx=.5,rely=.3,anchor=CENTER)
-Achievements=Button(app,text="Achievements",pady=20,padx=20,width=12,borderwidth = 10,command=achievements).place(relx=.5,rely=.4,anchor=CENTER)
-userInfo=Button(app,text="User Info",pady=20,padx=20,width=12,borderwidth = 10,command=userInfo).place(relx=.5,rely=.5,anchor=CENTER)
-History=Button(app,text="History",pady=20,padx=20,width=12,borderwidth = 10,command=history).place(relx=.5,rely=.6,anchor=CENTER)
+HomePage2=Label(frame1,text='Home Page',font=('bold',35),bg="#09c").place(relx=.5, rely=.2,anchor= CENTER)
+
+Exersises=Button(frame1,text="Exersises",pady=20,padx=20,width=12,borderwidth = 10,command=exersises).place(relx=.5,rely=.3,anchor=CENTER)
+Achievements=Button(frame1,text="Achievements",pady=20,padx=20,width=12,borderwidth = 10,command=achievements).place(relx=.5,rely=.4,anchor=CENTER)
+userInfo=Button(frame1,text="User Info",pady=20,padx=20,width=12,borderwidth = 10,command=userInfo).place(relx=.5,rely=.5,anchor=CENTER)
+History=Button(frame1,text="History",pady=20,padx=20,width=12,borderwidth = 10,command=history).place(relx=.5,rely=.6,anchor=CENTER)
 
 app.mainloop()
